4712d579c1cf237d101a516fcfd2a5e8f70b88b7
Elias Little <eliasjlittle@gmail.com>
2025-02-06 17:35:00 -0500

    Updated mod file

--- src/frontend/mod.rs
+++ src/frontend/mod.rs
@@ -1,4 +1,11 @@
 pub mod lexer;
 pub mod parser;
+pub mod ast;
+pub mod resolver;
 pub mod lower;
-pub mod ast; 
+
+pub use lexer::*;
+pub use parser::*;
+pub use ast::*;
+pub use resolver::*;
+pub use lower::*; 
